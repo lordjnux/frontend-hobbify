dde3c63e8d7601e851486856b4bdab44f53417fc
RivarolaFG <fer.rivarolag@gmail.com>
2024-06-25 15:55:30 -0300

    inicio del proyecto

---
+++ App.js
@@ -0,0 +1,7 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import HomeScreen from "./src/screens/HomeScreen";
+
+export default function App() {
+  return <HomeScreen />;
+}
