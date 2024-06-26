2d8e76db7e6bddb36aa94578e3ec837edc024030
EnriqueCanoli <enriquecanoli96@gmail.com>
2024-06-26 12:50:52 -0600

    separate components and styles

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import HomeScreen from "./src/screens/HomeScreen";
-import Perfil from "./src/screens/Perfil";
+import PerfilScreen from "./src/screens/Perfil";
 
 export default function App() {
-  return <Perfil />;
+  return <PerfilScreen />;
 }
