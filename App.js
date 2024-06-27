cbc6bf83cc3510b6203c4ee976c5951777d71dab
Camila Castro <cami@Camilas-MacBook-Air.local>
2024-06-27 07:37:02 -0300

    Corrección de una instalación innecesaria

--- App.js
+++ App.js
@@ -2,7 +2,8 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import HomeScreen from "./src/screens/HomeScreen";
 import Register from "./src/screens/Register";
+import Login from "./src/screens/Login";
 
 export default function App() {
-  return <Register />;
+  return <Login />;
 }
